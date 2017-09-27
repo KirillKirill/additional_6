3ce1d772102ba52667131b47894bb66a78debde2
Kirill Bochkarev <k.bochkarev@vironit.co.uk>
2017-09-28 00:42:21 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+  expression = expression.split('*');
+  var count2 = 0;
+  var count5 = 0;
+  for ( var i = 0; i < expression.length; i++){
+      if (expression[i].indexOf('!!') === -1){
+          expression[i] = Number(expression[i].slice(0,-1));
+          for (var j = 1; j <= expression[i]; j++){
+              if(j % 2 === 0) count2++;
+              if(j % 5 === 0 && j % 25 !== 0) count5++;
+              if(j % 25 === 0) count5 += 2;
+          }
+      }
+      else {
+          expression[i] = Number(expression[i].slice(0,-2));
+          if (expression[i] % 2 === 0){
+              for (j = 2; j <= expression[i]; j += 2){
+                  if(j % 2 === 0) count2++;
+                  if(j % 5 === 0 && j % 25 !== 0) count5++;
+                  if(j % 25 === 0) count5 += 2;
+              }
+          }
+          else {
+              for (j = 1; j <= expression[i]; j += 2){
+                  if(j % 2 === 0) count2++;
+                  if(j % 5 === 0 && j % 25 !== 0) count5++;
+                  if(j % 25 === 0) count5 += 2;
+              }
+          }
+      }
+  }
+  return Math.min(count2, count5);
+};
